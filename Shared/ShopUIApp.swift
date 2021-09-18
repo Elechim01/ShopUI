92a15d87833340d70f00846caa3a2165966b37a8
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-18 22:08:12 +0200

    Initial Commit

---
+++ Shared/ShopUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopUIApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 18/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ShopUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
